72c35ad27d7ecd91c2b67ec74de860ba80f87630
Uday Wahi <hiudaywahi@gmail.com>
2023-09-08 03:57:57 +0530

    move db.js to index to connect DB before listening

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
 const { json } = require("express");
+const mongoose = require("mongoose");
 
-// connectToMongo();
 const app = express();
+const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
 const hostname = "0.0.0.0";
 
@@ -22,8 +22,31 @@ app.get("/", (req, res) => {
   res.send("<h1>API is sending response</h1>");
 });
 
+const connectToMongo = async () => {
+  //mongoose connection options
+  option = {
+    serverSelectionTimeoutMS: 1000,
+    socketTimeoutMS: 60000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  // starting the connection to mongoDB
+  try {
+    const conn = await mongoose.connect(mongoURI, option);
+    console.log("Connected to mongoDB successfully");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+//Event emitters handling on mongoose client
+mongoose.connection.once("disconnected", () => {
+  console.log("Connection with mongoDB broken");
+});
+
 // Connect to the database before listening
-connectToMongo.then(() => {
+connectToMongo().then(() => {
   // starting the server
   app.listen(port, hostname, () => {
     console.log(`iNoteBook server http://${hostname}:${port}`);
